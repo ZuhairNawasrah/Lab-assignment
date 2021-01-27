e7423d2fac543529d87c8b49d606504d78313111
Zuhair.N <Zuhairnawasrah@gmail.com>
2021-01-27 16:55:45 +0300

    edit

--- Lab-4.js
+++ Lab-4.js
@@ -1,9 +1,9 @@
 var Language = prompt("what type of do you want to learn");
 var Lang;
 
-if (Language == HTML) {
+if (Language == "HTML") {
     Lang = "Press the first link in nav. bar";
-} else if (Language == CSS) {
+} else if (Language == "CSS") {
     Lang = "Press the second link in nav. bar";
 } else {
     Lang = "Please re-enter the language name";
