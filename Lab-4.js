2c8825a2859dc9f26531adf24034e641f1a20a79
Zuhair.N <Zuhairnawasrah@gmail.com>
2021-01-30 01:12:25 +0300

    best version ever

--- Lab-4.js
+++ Lab-4.js
@@ -1,9 +1,10 @@
 var Language = prompt("Which of 'HTML' or 'CSS' you wanna learn first?");
 var link;
-var sum;
+var sum = '';
+var pic = '<img src="HTML.png" width= 20px/>';
 
 while (Language !== "HTML" && Language !== "CSS") {
-    Language = prompt("Can you re-enter the answer?!");
+    Language = prompt("Can you re-enter the answer 'HTML' or 'CSS'?!");
 }
 for (var i = 0; i < 2;) {
     if (Language === "HTML" || Language === "CSS") {
@@ -14,15 +15,15 @@ for (var i = 0; i < 2;) {
 
         } else {
             alert("Your answer is canceled!");
-            Language = prompt("Can you re-enter the answer?!");
+            Language = prompt("Can you re-enter the answer 'HTML' or 'CSS'?!");
             while (Language !== "HTML" && Language !== "CSS") {
-                Language = prompt("Can you re-enter the answer?!");
+                Language = prompt("Can you re-enter the answer 'HTML' or 'CSS'?!");
             }
         }
     }
 }
 
-var pic = '<img src="HTML.png" width= 20px/>';
+
 var month_no = prompt("How many month do you want to finish this coures");
 alert("Good luck with coding journey");
 for (i = 0; i < month_no; i++) {
