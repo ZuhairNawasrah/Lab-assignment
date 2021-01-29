50fda6ad4c75f7b8bbe7cca85288ec01c8003af0
Zuhair.N <Zuhairnawasrah@gmail.com>
2021-01-30 00:35:51 +0300

    the best version ever

--- Lab-4.js
+++ Lab-4.js
@@ -1,53 +1,30 @@
-var Language = prompt("what type of language do you want to learn");
-var cancel = "Selection is canceled";
+var Language = prompt("Which of 'HTML' or 'CSS' you wanna learn first?");
 var link;
 var sum;
 
 while (Language !== "HTML" && Language !== "CSS") {
     Language = prompt("Can you re-enter the answer");
 }
-if (Language == "HTML") {
-    if (confirm("Are you sure?")) {
-        alert("Good luck with coding journey");
-        link = "Click 1st link";
-        document.write(link);
-    }
-    else {
-        Language = prompt("what type of language do you want to learn");
-        if (Language == "CSS") {
-            if (confirm("Are you sure?")) {
-                alert("Good luck with coding journey");
-                link = "Click 2nd link";
-                document.write(link);
-            } else {
-                document.write(cancel);
-            }
-        }
-    }
-} else {
-    if (Language == "CSS") {
+for (var i = 0; i < 2;) {
+    if (Language === "HTML" || Language === "CSS") {
         if (confirm("Are you sure?")) {
-            alert("Good luck with coding journey");
-            link = "Click 2nd link";
+            link = "Click " + Language + " Link";
             document.write(link);
-        }
-        else {
-            Language = prompt("what type of language do you want to learn");
-            if (Language == "HTML") {
-                if (confirm("Are you sure?")) {
-                    alert("Good luck with coding journey");
-                    link = "Click 1st link"
-                    document.write(link);
-                } else {
-                    document.write(cancel);
-                }
+            i = 2;
+
+        } else {
+            alert("Your answer is canceled!");
+            Language = prompt("Can you re-enter the answer?!");
+            while (Language !== "HTML" && Language !== "CSS") {
+                Language = prompt("Can you re-enter the answer?!");
             }
         }
     }
 }
+
 var pic = '<img src="HTML.png" width= 20px/>';
 var month_no = prompt("How many month do you want to finish this coures");
-
+alert("Good luck with coding journey");
 for (i = 0; i < month_no; i++) {
     sum = sum + pic;
 }
