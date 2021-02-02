b3ebce1815f30f955d74503531a8b399e8b1e6ea
Zuhair.N <Zuhairnawasrah@gmail.com>
2021-02-03 02:00:07 +0300

    new design

--- Lab-4.js
+++ Lab-4.js
@@ -1,14 +1,13 @@
 var Language = prompt("Which of 'HTML' or 'CSS' you wanna learn first?");
 var link;
-var sum = '';
 var pic = '<img src="HTML.png" width= 20px/>';
 
-input();
 function input() {
     while (Language !== "HTML" && Language !== "CSS") {
         Language = prompt("Can you re-enter the answer 'HTML' or 'CSS'?!");
     }
 }
+input();
 for (var i = 0; i < 2;) {
     if (Language === "HTML" || Language === "CSS") {
         if (confirm("Are you sure?")) {
@@ -24,11 +23,3 @@ for (var i = 0; i < 2;) {
     }
 }
 
-
-
-var month_no = prompt("How many months do you wanna finish this coures?");
-alert("Good luck with coding journey");
-for (i = 0; i < month_no; i++) {
-    sum = sum + pic;
-}
-document.write(sum);
